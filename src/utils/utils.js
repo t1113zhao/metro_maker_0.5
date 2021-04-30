eb432edc63062c0a4b56810449f58869fed94753
t1113zhao <zhaotian1998@gmail.com>
2021-04-29 21:24:12 -0400

    Generic Delete/Restore in Utils
    Generic Single/Multi Delete/Restore

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,3 +2,58 @@ export function nextIDForArray(array){
     const maxID = array.reduce((maxID, element) => Math.max(element.id, maxID),-1)
     return maxID + 1
 };
+
+export function genericSingleDelete(array, id){
+    return array.map(item => {
+        if (item.id != id) {
+            return item
+        }
+        return {
+            ...item,
+            deletedAt: action.payload.deletedAt
+        }
+    })
+}
+
+export function genericSingleRestore(array, id){
+    return array.map(item => {
+        if (item.id != id) {
+            return item
+        }
+        return {
+            ...item,
+            deletedAt: null
+        }
+    })
+}
+
+export function genericMultiDelete(array, ids, deletedAt){
+    var removeSet = new Set(ids)
+
+    return array.map((item) => {
+            if (removeSet.has(item.id)) {
+                return {
+                    ...item,
+                    deletedAt: deletedAt
+                }
+            } else {
+                return item
+            }
+        })
+}
+
+export function genericMultiRestore(array, ids){
+    var restoreSet = new Set(ids)
+
+    return array.map((item) => {
+            if (restoreSet.has(item.id)) {
+                return {
+                    ...item,
+                    deletedAt: null
+                }
+            } else {
+                return item
+            }
+        })
+
+}
